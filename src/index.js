92c68dff69e26f65c26895512ba92eef0dc11916
Jon Dawson <dawson.jon@hotmail.com>
2021-10-03 16:16:01 +0100

    added routes to App.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ import DisplayingDataFromAnAPI from './lessons/DisplayingDataFromAnAPI'
 import RenderComponentsConditionally from './lessons/RenderComponentsConditionally'
 import HandlingLoadingStates from './lessons/HandlingLoadingStates'
 import AppTest from './AppTest'
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<AppTest />, document.getElementById('root'));
+ReactDOM.render(
+  <Router> 
+    <App />  
+  </Router>,
+  document.getElementById('root')
+);
 
 // React.Strictmode removed 
 // This is best practices for syntax when using React and will throw errors if bad practice
